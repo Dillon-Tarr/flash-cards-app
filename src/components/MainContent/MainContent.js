0de1b24d1cb6d97d5ae9c93d79f0a7f8a94cd98c
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-09-29 18:58:45 -0400

    added axios request and filled MainContent

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function MainContent() {
+export default function MainContent(props) {
   return (
-    <div>
-      
+    <div id="main-content">
+      <h1 id="main-content-title">{props.mainContentTitle}</h1>
     </div>
   )
 }
