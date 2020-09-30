f8c8d3df1fdcdcb91f21e8ff386af425e839d446
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-09-29 21:45:56 -0400

    made collection buttons look good

--- src/components/CollectionButton/CollectionButton.js
+++ src/components/CollectionButton/CollectionButton.js
@@ -3,10 +3,12 @@ import React from 'react'
 export default function CollectionButton(props) {
   return (
     <button>
-      <div className="card card-main-back"></div>
-      <div className="card card-main-middle"></div>
-      <div className="card card-main-front">
-        <p>{props.collectionTitle}</p>
+      <div className="collection-container-main">
+        <div className="card card-main-back"></div>
+        <div className="card card-main-middle"></div>
+        <div className="card card-main-front">
+          <p>{props.collectionTitle}</p>
+        </div>
       </div>
     </button>
   )
