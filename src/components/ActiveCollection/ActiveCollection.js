6227436a0d70eab19a5abd739fd55d48879f3853
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-09-30 21:51:47 -0400

    made active card flippable and made it look nicer

--- src/components/ActiveCollection/ActiveCollection.js
+++ src/components/ActiveCollection/ActiveCollection.js
@@ -10,27 +10,40 @@ export default class ActiveCollection extends Component {
       displayWord: false
     }
   }
+
+  render() {
+    return (
+      <div id="active-collection-cards">
+        <button onClick={this.flipCard}>
+          <div className="card" id="card-active-back"></div>
+          <div className="card" id="card-active-middle"></div>
+          <div className="card" id="card-active-front">
+          {this.renderWordOrDefinition()}
+          <p id="flip-card-button-holder">Flip Card</p>
+        </div>
+        </button>
+      </div>
+    )
+  }
+
   renderWordOrDefinition(){
     if(this.state.displayWord === false){
       return (
-        <p id= "word">{this.state.currentCard.word}</p>
+        <p id="word">{this.state.currentCard.word}</p>
       )
     }
     else{
       return(
-        <p id= "definition">{this.state.currentCard.definition}</p>
+        <p id="definition">{this.state.currentCard.definition}</p>
       )
     }
   }
-  render() {
-    return (
-      <div id="active-collection-cards">
-        <div className="card" id="card-active-back"></div>
-        <div className="card" id="card-active-middle"></div>
-        <div className="card" id="card-active-front">
-          {this.renderWordOrDefinition()}
-        </div>
-      </div>
-    )
+
+  flipCard = () => {
+    let newDisplayState = !this.state.displayWord;
+    this.setState({
+      displayWord: newDisplayState
+    });
   }
+
 }
